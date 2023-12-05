9aef63639988c2d9cd7f6cf903f92d65df80b90f
Alejandro Ruiz <alejandroruizprieto@alumnos.tuniversformacion.es>
2023-12-05 14:00:35 +0100

    Final

--- scripts/main.js
+++ scripts/main.js
@@ -10,19 +10,23 @@ const productManager = new ProductManager();
 document.getElementById('product-form-events').addEventListener('submit', (event) => {
     event.preventDefault();
 
-    //Obtenemos los valores del formulario
-    const id = document.getElementById('id').value;
-    const nombre = document.getElementById('nombre').value;
-    const cantidad = document.getElementById('cantidad').value;
-    const precio = document.getElementById('precio').value;
+    const id = document.getElementById('product-name').value;
+    const productPrice = document.getElementById('product-price').value;
+    const productQuantity = document.getElementById('product-quantity').value;
+    
+    //Crear una instancia de Product con los valores del formulario
+    const newProduct = new Product(Date.now(), productName, productPrice, productQuantity);
 
-    const product = new Product(id, nombre, cantidad, precio);
-
-    productManager.addProduct(product);
+    //agrega el nuevo producto al administrador de productos
+    productManager.addProduct(newProduct);
 
     //Limpiar el formulario
-    document.getElementById('product-form-events').reset();
+    this.reset();
 
-    //Mostrar la lista de productos
-    productManager.showProducts();
+
+    //Actualizamos la tabla de inventario
+    updateInventoryTable();
+
+    
+
 });
